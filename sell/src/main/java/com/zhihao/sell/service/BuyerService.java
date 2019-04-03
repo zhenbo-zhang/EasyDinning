e7f63aecd4e70a17176fad9314b6a5c0694a706b
zhihaozhangim <zhihaozhangim@gmail.com>
2019-04-03 13:20:05 -0700

    find and cancel order with openid and order id

--- sell/src/main/java/com/zhihao/sell/service/BuyerService.java
+++ sell/src/main/java/com/zhihao/sell/service/BuyerService.java
@@ -1,13 +1,15 @@
 package com.zhihao.sell.service;
 
-import com.imooc.sell.dto.OrderDTO;
 import com.zhihao.sell.dto.OrderDTO;
 
+/**
+ * Interface BuyerService used to find or cancel an order.
+ */
 public interface BuyerService {
 
-    // Check an Order
-    OrderDTO findOrderOne(String openid, String orderId);
+  // Check an Order
+  OrderDTO findOrderOne(String openid, String orderId);
 
-    // Cancel an Order
-    OrderDTO cancelOrder(String openid, String orderId);
+  // Cancel an Order
+  OrderDTO cancelOrder(String openid, String orderId);
 }
--- sell/src/main/java/com/zhihao/sell/service/impl/BuyerServiceImpl.java
+++ sell/src/main/java/com/zhihao/sell/service/impl/BuyerServiceImpl.java
@@ -1,10 +1,6 @@
 package com.zhihao.sell.service.impl;
 
-import com.imooc.sell.dto.OrderDTO;
-import com.imooc.sell.enums.ResultEnum;
-import com.imooc.sell.exception.SellException;
-import com.imooc.sell.service.BuyerService;
-import com.imooc.sell.service.OrderService;
+import com.zhihao.sell.exception.SellException;
 import com.zhihao.sell.dto.OrderDTO;
 import com.zhihao.sell.enums.ResultEnum;
 import com.zhihao.sell.service.BuyerService;
@@ -13,37 +9,61 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+/**
+ * Class BuyerService used to find or cancel an order.
+ */
 @Service
 @Slf4j
 public class BuyerServiceImpl implements BuyerService {
 
-    @Autowired
-    private OrderService orderService;
+  @Autowired
+  private OrderService orderService;
 
-    @Override
-    public OrderDTO findOrderOne(String openid, String orderId) {
-        return checkOrderOwner(openid, orderId);
-    }
+  /**
+   * Find the order with the openid and order id.
+   *
+   * @param openid - the open id
+   * @param orderId - the order id
+   * @return the order with the openid and order id
+   */
+  @Override
+  public OrderDTO findOrderOne(String openid, String orderId) {
+    return checkOrderOwner(openid, orderId);
+  }
 
-    @Override
-    public OrderDTO cancelOrder(String openid, String orderId) {
-        OrderDTO orderDTO = checkOrderOwner(openid, orderId);
-        if (orderDTO == null) {
-            log.error("【Cancel Order】Cannot Find the Order，orderId={}", orderId);
-            throw new SellException(ResultEnum.ORDER_NOT_EXIST);
-        }
-        return orderService.cancel(orderDTO);
+  /**
+   * Cancel the order with the openid and order id.
+   *
+   * @param openid - the open id
+   * @param orderId - the order id
+   * @return the canceled order with the openid and order id
+   */
+  @Override
+  public OrderDTO cancelOrder(String openid, String orderId) {
+    OrderDTO orderDTO = checkOrderOwner(openid, orderId);
+    if (orderDTO == null) {
+      log.error("【Cancel Order】Cannot Find the Order，orderId={}", orderId);
+      throw new SellException(ResultEnum.ORDER_NOT_EXIST);
     }
+    return orderService.cancel(orderDTO);
+  }
 
-    private OrderDTO checkOrderOwner(String openid, String orderId) {
-        OrderDTO orderDTO = orderService.findOne(orderId);
-        if (orderDTO == null) {
-            return null;
-        }
-        if (!orderDTO.getBuyerOpenid().equalsIgnoreCase(openid)) {
-            log.error("【Check Order】Wrong Open Id. openid={}, orderDTO={}", openid, orderDTO);
-            throw new SellException(ResultEnum.ORDER_OWNER_ERROR);
-        }
-        return orderDTO;
+  /**
+   * Helper method used by the above methods.
+   *
+   * @param openid - the open id
+   * @param orderId - the order id
+   * @return the order with the openid and order id
+   */
+  private OrderDTO checkOrderOwner(String openid, String orderId) {
+    OrderDTO orderDTO = orderService.findOne(orderId);
+    if (orderDTO == null) {
+      return null;
     }
+    if (!orderDTO.getBuyerOpenid().equalsIgnoreCase(openid)) {
+      log.error("【Check Order】Wrong Open Id. openid={}, orderDTO={}", openid, orderDTO);
+      throw new SellException(ResultEnum.ORDER_OWNER_ERROR);
+    }
+    return orderDTO;
+  }
 }
